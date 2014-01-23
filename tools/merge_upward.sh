e1a1170690917ba97685b4a1c91e0b7fa36726e4
Benjamin Porter <FreedomBen@users.noreply.github.com>
2014-01-22 21:40:32 -0700

    merge tool Tweak syntax

--- tools/merge_upward.sh
+++ tools/merge_upward.sh
@@ -2,24 +2,16 @@
 
 git checkout -f ubuntu-13.04        && \
                                        \
-&&                                     \
-                                       \
 git merge origin/ubuntu-12.04       && \
-git push                               \
-                                       \
-&&                                     \
+git push                            && \
                                        \
 git checkout -f ubuntu-13.10        && \
 git merge origin/ubuntu-13.04       && \
-git push                               \
-                                       \
-&&                                     \
+git push                            && \
                                        \
 git checkout -f fedora-20           && \
 git merge origin/ubuntu-13.10       && \
-git push                               \
-                                       \
-&&                                     \
+git push                            && \
                                        \
 git checkout -f master              && \
 git merge origin/fedora-20          && \
