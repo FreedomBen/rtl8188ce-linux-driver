1defbb772be444184ee5fd0047ef1763e21efded
Benjamin Porter <FreedomBen@users.noreply.github.com>
2014-05-02 09:16:38 -0600

    rtl8192c Update Makefile and main.c

--- rtl8192c/main.c
+++ rtl8192c/main.c
@@ -36,5 +36,6 @@ MODULE_AUTHOR( "Realtek WlanFAE	<wlanfae@realtek.com>" );
 MODULE_AUTHOR( "Georgia		<georgia@realtek.com>" );
 MODULE_AUTHOR( "Ziv Huang	<ziv_huang@realtek.com>" );
 MODULE_AUTHOR( "Larry Finger	<Larry.Finger@lwfinger.net>" );
+MODULE_AUTHOR( "Benjamin Porter	<BenjaminPorter86@gmail.com>" );
 MODULE_LICENSE( "GPL" );
 MODULE_DESCRIPTION( "Realtek 8192C/8188C 802.11n PCI wireless" );
