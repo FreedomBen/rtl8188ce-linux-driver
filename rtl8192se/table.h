9c80b8ab62a42b2374948ef072e7a6ea7e7654cd
Benjamin Porter <FreedomBen@users.noreply.github.com>
2014-08-01 22:26:46 -0600

    Restore header comment that was lost

--- rtl8192se/table.h
+++ rtl8192se/table.h
@@ -18,6 +18,13 @@
  *
  * Larry Finger <Larry.Finger@lwfinger.net>
  *
+ *
+ * Bug Fixes and enhancements for Linux Kernels >= 3.2
+ * by Benjamin Porter <BenjaminPorter86@gmail.com>
+ *
+ * Project homepage: https://github.com/FreedomBen/rtl8188ce-linux-driver
+ *
+ *
  ******************************************************************************/
 #ifndef __INC_HAL8192SE_FW_IMG_H
 #define __INC_HAL8192SE_FW_IMG_H
