3e7cb66d2229141f6b09a372d1293098ea0aaa73
Benjamin Porter <FreedomBen@users.noreply.github.com>
2014-08-14 20:02:06 -0600

    Mint17 trx.c use more complex form of _ieee80211_is_robust_mgmt_frame

--- rtl8723be/trx.c
+++ rtl8723be/trx.c
@@ -602,7 +602,7 @@ bool rtl8723be_rx_query_desc( struct ieee80211_hw *hw,
 			return false;
 		}
 
-		if ( ( _ieee80211_is_robust_mgmt_frame( hdr ) ) &&
+		if ( ( ieee80211_is_robust_mgmt_frame( hdr ) ) &&
 		    ( ieee80211_has_protected( hdr->frame_control ) ) )
 			rx_status->flag &= ~RX_FLAG_DECRYPTED;
 		else
