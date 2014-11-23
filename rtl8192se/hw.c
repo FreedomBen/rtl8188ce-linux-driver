46d9a15272f99c7aefa6eabe55aeaf003585b578
Benjamin Porter <FreedomBen@users.noreply.github.com>
2014-11-23 09:02:25 -0700

    Safer IRQ handling for 8188ee, 8192cu, 8192se, and 8723ae
    We're screwing up the interrupt requests by taking too long, so re-enable
    irqs to let other devices continue being serviced
    
    Refs #54
    Refs #55

--- rtl8192se/hw.c
+++ rtl8192se/hw.c
@@ -962,7 +962,7 @@ int rtl92se_hw_init( struct ieee80211_hw *hw )
 	struct rtl_pci *rtlpci = rtl_pcidev( rtl_pcipriv( hw ) );
 	struct rtl_efuse *rtlefuse = rtl_efuse( rtl_priv( hw ) );
 	u8 tmp_byte = 0;
-
+	unsigned long flags;
 	bool rtstatus = true;
 	u8 tmp_u1b;
 	int err = false;
@@ -973,6 +973,16 @@ int rtl92se_hw_init( struct ieee80211_hw *hw )
 	u8 secr_value = 0x0;
 
 	rtlpci->being_init_adapter = true;
+
+	/* As this function can take a very long time ( up to 350 ms )
+	 * and can be called with irqs disabled, reenable the irqs
+	 * to let the other devices continue being serviced.
+	 *
+	 * It is safe doing so since our own interrupts will only be enabled
+	 * in a subsequent step.
+	 */
+	local_save_flags( flags );
+	local_irq_enable();
 
 	rtlpriv->intf_ops->disable_aspm( hw );
 
@@ -991,7 +1001,8 @@ int rtl92se_hw_init( struct ieee80211_hw *hw )
 		RT_TRACE( rtlpriv, COMP_ERR, DBG_WARNING,
 			 "Failed to download FW. Init HW without FW now... "
 			 "Please copy FW into /lib/firmware/rtlwifi\n" );
-		return 1;
+		err = 1;
+		goto exit;
 	}
 
 	/* After FW download, we have to reset MAC register */
@@ -1004,7 +1015,8 @@ int rtl92se_hw_init( struct ieee80211_hw *hw )
 	/* 3. Initialize MAC/PHY Config by MACPHY_reg.txt */
 	if ( !rtl92s_phy_mac_config( hw ) ) {
 		RT_TRACE( rtlpriv, COMP_ERR, DBG_EMERG, "MAC Config failed\n" );
-		return rtstatus;
+		err = rtstatus;
+		goto exit;
 	}
 
 	/* because last function modify RCR, so we update
@@ -1023,7 +1035,8 @@ int rtl92se_hw_init( struct ieee80211_hw *hw )
 	/* 4. Initialize BB After MAC Config PHY_reg.txt, AGC_Tab.txt */
 	if ( !rtl92s_phy_bb_config( hw ) ) {
 		RT_TRACE( rtlpriv, COMP_INIT, DBG_EMERG, "BB Config failed\n" );
-		return rtstatus;
+		err = rtstatus;
+		goto exit;
 	}
 
 	/* 5. Initiailze RF RAIO_A.txt RF RAIO_B.txt */
@@ -1040,7 +1053,8 @@ int rtl92se_hw_init( struct ieee80211_hw *hw )
 
 	if ( !rtl92s_phy_rf_config( hw ) ) {
 		RT_TRACE( rtlpriv, COMP_INIT, DBG_DMESG, "RF Config failed\n" );
-		return rtstatus;
+		err = rtstatus;
+		goto exit;
 	}
 
 	/* After read predefined TXT, we must set BB/MAC/RF
@@ -1129,8 +1143,9 @@ int rtl92se_hw_init( struct ieee80211_hw *hw )
 
 	rtlpriv->cfg->ops->led_control( hw, LED_CTL_POWER_ON );
 	rtl92s_dm_init( hw );
+exit:
+	local_irq_restore( flags );
 	rtlpci->being_init_adapter = false;
-
 	return err;
 }
 
