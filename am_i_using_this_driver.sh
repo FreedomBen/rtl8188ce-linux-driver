18811e73f8e25ac99bb8639896368e77a6a6bcf3
Benjamin Porter <FreedomBen@users.noreply.github.com>
2014-12-04 21:13:33 -0700

    Add starting support for detecting multiple cards running

--- am_i_using_this_driver.sh
+++ am_i_using_this_driver.sh
@@ -7,33 +7,98 @@ fi
 
 . "$(readlink -f functions.sh)"
 
+runningNew ()
+{
+    echo -e "${green}[*] You are running the new ${1}${restore}"
+}
+
+runningStock ()
+{
+
+    echo -e "${yellow}[*] Not running the new $1 (running stock driver)${restore}"
+}
+
+runningNone ()
+{
+    echo -e "${red}[*] Not running any ${1}${restore}"
+}
+
 if runningAnyRtlwifi; then
     if runningOurRtlwifi; then
-        echo -e "${green}[*] You are running the new rtlwifi${restore}"
+        runningNew "rtlwifi"
     else
-        echo -e "${yellow}[*] Not running the new rtlwifi (running stock driver)${restore}"
+        runningStock "rtlwifi"
     fi
 else
-    echo -e "${red}[*] Not running any rtlwifi${restore}"
+    runningNone "rtlwifi"
 fi
 
-if runningAnyRtl8192c_common; then
-    if runningOurRtl8192c_common; then
-        echo -e "${green}[*] You are running the new rtl8192c_common${restore}"
+if pciDetectsRtl8188ce || pciDetectsRtl8192ce; then
+    if runningAnyRtl8192c_common; then
+        if runningOurRtl8192c_common; then
+            runningNew "rtl8192c_common"
+        else
+            runningStock "rtl8192c_common"
+        fi
     else
-        echo -e "${yellow}[*] Not running the new rtl8192c_common (running stock driver)${restore}"
+        runningNone "rtl8192c_common"
     fi
-else
-    echo -e "${red}[*] Not running any rtl8192c_common${restore}"
-fi
 
-if runningAnyRtl8192ce; then
-    if runningOurRtl8192ce; then
-        echo -e "${green}[*] You are running the new rtl8192ce${restore}"
+    if runningAnyRtl8192ce; then
+        if runningOurRtl8192ce; then
+            runningNew "rtl8192ce"
+        else
+            runningStock "rtl8192ce"
+        fi
     else
-        echo -e "${yellow}[*] Not running the new rtl8192ce (running stock driver)${restore}"
+        runningNone "rtl8192ce"
     fi
-else
-    echo -e "${red}[*] Not running any rtl8192ce${restore}"
+elif runningAnyRtl8188ee; then
+    if runningOurRtl8188ee; then
+        runningNew "rtl8188ee"
+    else
+        runningStock "rtl8188ee"
+    fi
+elif runningAnyRtl8192cu; then
+    if runningOurRtl8192cu; then
+        runningNew "rtl8192cu"
+    else
+        runningStock "rtl8192cu"
+    fi
+elif runningAnyRtl8192de; then
+    if runningOurRtl8192de; then
+        runningNew "rtl8192de"
+    else
+        runningStock "rtl8192de"
+    fi
+elif runningAnyRtl8192ee; then
+    if runningOurRtl8192ee; then
+        runningNew "rtl8192ee"
+    else
+        runningStock "rtl8192ee"
+    fi
+elif runningAnyRtl8192se; then
+    if runningOurRtl8192se; then
+        runningNew "rtl8192se"
+    else
+        runningStock "rtl8192se"
+    fi
+elif runningAnyRtl8723ae; then
+    if runningOurRtl8723ae; then
+        runningNew "rtl8723ae"
+    else
+        runningStock "rtl8723ae"
+    fi
+elif runningAnyRtl8723be; then
+    if runningOurRtl8723be; then
+        runningNew "rtl8723be"
+    else
+        runningStock "rtl8723be"
+    fi
+elif runningAnyRtl8821ae; then
+    if runningOurRtl8821ae; then
+        runningNew "rtl8821ae"
+    else
+        runningStock "rtl8821ae"
+    fi
 fi
-
