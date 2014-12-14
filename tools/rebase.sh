4c0d395277c30b3773cb655e98f1473b74c1b179
Benjamin Porter <FreedomBen@users.noreply.github.com>
2014-12-13 21:35:37 -0700

    Add rebase tool

---
+++ tools/rebase.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+LOCATION="$HOME/gitclone"
+
+GENERIC='git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git'
+UBUNTU1404='git://kernel.ubuntu.com/ubuntu/ubuntu-trusty.git'
+UBUNTU1410='git://kernel.ubuntu.com/ubuntu/ubuntu-utopic.git'
+
+starting_dir="$(pwd)"
+
+die ()
+{
+    echo "[DIE]: $1" 2>&1
+    exit 1
+}
+
+is_generic ()
+{
+    echo "$1" | grep "generic" >/dev/null 2>&1
+}
+
+is_ubuntu ()
+{
+    echo "$1" | grep "ubuntu" >/dev/null 2>&1
+}
+
+tag ()
+{
+    if $(is_generic $1); then
+        if $(echo "$1" | grep "3\.18\." >/dev/null 2>&1); then
+            echo "v3.18"
+            return
+        elif $(echo "$1" | grep "3\.17\." >/dev/null 2>&1); then
+            echo "v3.17.6"
+            return
+        fi
+    elif $(is_ubuntu $1); then
+        if $(echo "$1" | grep "14.04" >/dev/null 2>&1); then
+            echo "Ubuntu-3.13.0-9.29"
+            return
+        elif $(echo "$1" | grep "14.10" >/dev/null 2>&1); then
+            echo "Ubuntu-3.16.0-9.14"
+            return
+        fi
+    fi
+    die "unknown tag for \"$1\""
+}
+
+repo_dir ()
+{
+    echo $1 | sed -e 's/.*\///g' | sed -e 's/\.git//g'
+}
+
+[ -f "regd.c" ] || die "Need to start script from root of dir"
+
+[ -n "$1" ] || die "Need branch to rebase specified"
+
+mkdir -p "$LOCATION"
+cd $LOCATION
+
+if [ "$1" = "ubuntu-14.04" ]; then
+    remote="$UBUNTU1404"
+elif [ "$1" = "ubuntu-14.10" ]; then
+    remote="$UBUNTU1410"
+elif [[ $1 =~ generic ]]; then
+    remote="$GENERIC"
+fi
+
+[ -n "$remote" ] || die "Uunsupported branch \"$1\""
+
+[ -d "$(repo_dir $remote)" ] || git clone "$remote"
+[ -d "$(repo_dir $remote)" ] || die "could not clone $remote and the dir does not exist"
+
+cd "$(repo_dir $remote)"
+git checkout --force master
+git pull --rebase || die "Problem running git pull"
+git checkout --force "$(tag $1)" || die "Problem checking out $(tag $1)"
+temp="$(pwd)"
+cd "$starting_dir"
+git checkout --force "$1"
+cd "$temp"
+cp -r drivers/net/wireless/rtlwifi/* "${starting_dir}/"
+
+cd "$starting_dir"
+
+git checkout Kconfig
+find . -name "Makefile" | xargs git checkout -f
+~/bin/fixParens.py *.[ch] **/*.[ch]
+./tools/addNameToCommentHeader.py *.[ch] **/*.[ch]
+
+echo "Ok, ready for manual merging"
