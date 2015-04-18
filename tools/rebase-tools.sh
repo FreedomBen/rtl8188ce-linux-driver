7826dec664291d4b7c71be303a6097a7608af205
Benjamin Porter <bporter@instructure.com>
2015-04-18 09:37:09 -0600

    Update tools to latest from master
        updated by the update script ./tools/rebase-tools.sh

--- tools/rebase-tools.sh
+++ tools/rebase-tools.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 files='*.sh README.md tools/'
-declare -a branches=('ubuntu-15.04' 'ubuntu-14.10' 'generic-3.19.x' 'mint-17.1' 'generic-3.18.x' 'generic-3.17.x' 'generic-3.13.x' 'mint-17' 'generic-3.16.x' 'ubuntu-12.04' 'ubuntu-13.04' 'ubuntu-13.10' 'ubuntu-14.04' 'fedora-20' 'arch' 'generic-3.14.x' 'fedora-19')
+declare -a branches=('generic-4.0.x' 'ubuntu-15.04' 'ubuntu-14.10' 'generic-3.19.x' 'mint-17.1' 'generic-3.18.x' 'generic-3.17.x' 'generic-3.13.x' 'mint-17' 'generic-3.16.x' 'ubuntu-12.04' 'ubuntu-13.04' 'ubuntu-13.10' 'ubuntu-14.04' 'fedora-20' 'arch' 'generic-3.14.x' 'fedora-19')
 
 restore='\033[0m'
 red='\033[0;31m'
