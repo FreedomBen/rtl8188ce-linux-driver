ace4575c2bf11ea26de9a5e20c76689b1579bc50
Benjamin Porter <bporter@instructure.com>
2015-04-17 21:14:44 -0600

    Rebase against 3.19.4

---
+++ btcoexist/btcoexist.mod.c
@@ -0,0 +1,33 @@
+#include <linux/module.h>
+#include <linux/vermagic.h>
+#include <linux/compiler.h>
+
+MODULE_INFO( vermagic, VERMAGIC_STRING );
+
+__visible struct module __this_module
+__attribute__( ( section( ".gnu.linkonce.this_module" ) ) ) = {
+	.name = KBUILD_MODNAME,
+	.init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+	.exit = cleanup_module,
+#endif
+	.arch = MODULE_ARCH_INIT,
+};
+
+MODULE_INFO( intree, "Y" );
+
+static const struct modversion_info ____versions[]
+__used
+__attribute__( ( section( "__versions" ) ) ) = {
+	{ 0xed5b2ed, __VMLINUX_SYMBOL_STR( module_layout ) },
+	{ 0xeae3dfd6, __VMLINUX_SYMBOL_STR( __const_udelay ) },
+	{ 0x27e1a049, __VMLINUX_SYMBOL_STR( printk ) },
+	{ 0xdb7305a1, __VMLINUX_SYMBOL_STR( __stack_chk_fail ) },
+	{ 0xbdfb6dbb, __VMLINUX_SYMBOL_STR( __fentry__ ) },
+};
+
+static const char __module_depends[]
+__used
+__attribute__( ( section( ".modinfo" ) ) ) =
+"depends=";
+
