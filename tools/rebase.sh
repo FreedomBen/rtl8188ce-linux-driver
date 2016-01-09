4bd88218c77efc5c71abbae8c44ef2fe1fd3f8de
Benjamin Porter <bporter@instructure.com>
2016-01-09 00:51:43 +0000

    Update tools to latest from master
        updated by the update script ./tools/rebase-tools.sh

--- tools/rebase.sh
+++ tools/rebase.sh
@@ -29,7 +29,10 @@ is_ubuntu ()
 tag ()
 {
     if $(is_generic $1); then
-        if $(echo "$1" | grep "4\.3" >/dev/null 2>&1); then
+        if $(echo "$1" | grep "4\.4" >/dev/null 2>&1); then
+            echo "$(git tag | grep "v4\.4" | sort -n | ~/extract.rb)"
+            return
+        elif $(echo "$1" | grep "4\.3" >/dev/null 2>&1); then
             echo "$(git tag | grep "v4\.3" | sort -n | ~/extract.rb)"
             return
         elif $(echo "$1" | grep "4\.2" >/dev/null 2>&1); then
@@ -109,7 +112,12 @@ temp="$(pwd)"
 cd "$starting_dir"
 git checkout --force "$1"
 cd "$temp"
-cp -r drivers/net/wireless/rtlwifi/* "${starting_dir}/"
+
+if [ -d "drivers/net/wireless/rtlwifi" ]; then
+    cp -r drivers/net/wireless/rtlwifi/* "${starting_dir}/"
+else
+    cp -r drivers/net/wireless/realtek/rtlwifi/* "${starting_dir}/"
+fi
 
 cd "$starting_dir"
 
