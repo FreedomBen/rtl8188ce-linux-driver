b13256d5391551e30a811eb9220a95a3e664c73a
Benjamin Porter <bporter@instructure.com>
2017-03-17 21:18:52 -0600

    Update tools for 4.11.x

--- verify_branch.sh
+++ verify_branch.sh
@@ -34,7 +34,9 @@ doSwitch ()
 echo "Verifying a sane branch for your kernel version..."
 
 if inGitRepo; then
-    if $(uname -r | grep "4.10" > /dev/null); then
+    if $(uname -r | grep "4.11" > /dev/null); then
+        doSwitch "generic-4.11.x"
+    elif $(uname -r | grep "4.10" > /dev/null); then
         doSwitch "generic-4.10.x"
     elif $(uname -r | grep "4.9" > /dev/null); then
         doSwitch "generic-4.9.x"
@@ -114,7 +116,8 @@ if inGitRepo; then
     fi
 else
     base="$(basename $(pwd))"
-    if ( $(uname -r | grep "4.10" > /dev/null) && ! $(echo "$base" | grep "generic-4.10.x" > /dev/null) ) ||                                                          \
+    if ( $(uname -r | grep "4.11" > /dev/null) && ! $(echo "$base" | grep "generic-4.11.x" > /dev/null) ) ||                                                          \
+       ( $(uname -r | grep "4.10" > /dev/null) && ! $(echo "$base" | grep "generic-4.10.x" > /dev/null) ) ||                                                          \
        ( $(uname -r | grep "4.9"  > /dev/null) && ! $(echo "$base" | grep "generic-4.9.x"  > /dev/null) ) ||                                                          \
        ( $(uname -r | grep "4.8"  > /dev/null) && ! $(echo "$base" | grep "generic-4.8.x"  > /dev/null) ) && ! $(echo "$base" | grep "ubuntu-16.10"   > /dev/null) || \
        ( $(uname -r | grep "4.7"  > /dev/null) && ! $(echo "$base" | grep "generic-4.7.x"  > /dev/null) ) ||                                                          \
